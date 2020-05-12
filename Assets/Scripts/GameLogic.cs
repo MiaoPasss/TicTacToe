9a1de72e47332acb8b7de814e3bdd4f13c558114
Haotian Luo <hubery.luo@icloud.com>
2020-05-12 17:57:20 -0400

    Basic Game Logic

---
+++ Assets/Scripts/GameLogic.cs
@@ -0,0 +1,37 @@
+﻿public class GameLogic
+{
+    private Player[] GameBoard = new Player[9];
+    public enum Player
+    {
+        empty = 0,
+        circle = 1,
+        cross = -1
+    }
+
+    public struct GameResult {
+        public readonly bool IsGameOver;
+        public readonly Player WinningPlayer;
+
+        public GameResult(bool isGameOver, Player winningPlayer)
+        {
+            IsGameOver = isGameOver;
+            WinningPlayer = winningPlayer;
+        }
+    }
+
+    public void Make_Move(Player player, int spot)
+    {
+        if (this.Check_Empty_Spot(spot))
+            GameBoard[spot] = player;
+    }
+
+    public GameResult Check_Game_Over()
+    {
+        return new GameResult(false, Player.empty);
+    }
+
+    private bool Check_Empty_Spot(int spot)
+    {
+        return GameBoard[spot] == Player.empty;
+    }
+}
--- Assets/Scripts/TicTacToeGame.cs
+++ Assets/Scripts/TicTacToeGame.cs
@@ -1,18 +1,10 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class TicTacToeGame : MonoBehaviour
+﻿public class GameLogic
 {
-    // Start is called before the first frame update
-    void Start()
+    private int[] BoardState = new int[9];
+
+    public GameLogic()
     {
         
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
         
     }
 }
