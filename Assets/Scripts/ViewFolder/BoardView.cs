6e8fa1bb55a5259cf9077946c5467f03c4ef0ee5
Haotian Luo <hubery.luo@icloud.com>
2020-05-13 19:36:55 -0400

    Drawing the background/board in Unity

---
+++ Assets/Scripts/ViewFolder/BoardView.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoardView : MonoBehaviour, IView
+{
+    public int[] Ask_For_Next_Move()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void Draw_GameOver(GameResultViewData game_result)
+    {
+        
+    }
+
+    public void Draw_Move(int row, int col)
+    {
+        
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
