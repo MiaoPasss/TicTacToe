02f35265fa84bcaaa43dae7fa9934b817a4841c9
Haotian Luo <hubery.luo@icloud.com>
2020-05-14 16:09:21 -0400

    Added individual callback function on each button

--- Assets/Scripts/ViewFolder/BoardView.cs
+++ Assets/Scripts/ViewFolder/BoardView.cs
@@ -1,9 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
 
 public class BoardView : MonoBehaviour, IView
 {
+    [SerializeField] private PrefabInstantiator ButtonInstantiator;
+    private Button[] ButtonGroup = new Button[9];
+
     public int[] Ask_For_Next_Move()
     {
         throw new System.NotImplementedException();
@@ -20,14 +25,24 @@ public class BoardView : MonoBehaviour, IView
     }
 
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-        
+        for (int i = 0; i < 9; i++)
+        {
+            ButtonGroup[i] = ButtonInstantiator.Instantiate().GetComponent<Button>();
+            int button_index = i;
+            UnityAction on_ith_button_clicked = () => 
+            {
+                Debug.Log(button_index + "-th button clicked");
+            };
+            RegisterButtonClick(ButtonGroup[i], on_ith_button_clicked);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void RegisterButtonClick(Button button_click, UnityAction onClick)
     {
-        
+        button_click.onClick.AddListener(onClick);
     }
+
+    
 }
---
+++ Assets/Scripts/ViewFolder/PrefabInstantiator.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class PrefabInstantiator : MonoBehaviour
+{
+    [SerializeField] private GameObject prefab;
+
+    public GameObject Instantiate()
+    {
+        return GameObject.Instantiate(prefab, transform);
+    }
+}
